19f86f47518ed302bac1907493c571b6f495a278
kartier3 <136193694+kartier3@users.noreply.github.com>
2025-11-27 17:42:14 +0000

    Improvedd Menu,
    Finish with the Main Menu and main functional functions

--- src/main/kotlin/Controllers/SupplierAPI.kt
+++ src/main/kotlin/Controllers/SupplierAPI.kt
@@ -8,17 +8,47 @@ class SupplierAPI {
     private val suppliers = mutableListOf<Supplier>()
 
     fun addSupplier(supplier: Supplier): Boolean { return suppliers.add(supplier) }
+
+
+
     fun getAllSuppliers(): List<Supplier> {return suppliers}
+    fun displaySuppliers(list: List<Supplier>) {
+        if (list.isEmpty()) {
+            println("No suppliers found.")
+        } else {
+            println("Suppliers:")
+            for (supplier in list) {
+                println("${supplier.supplierId}. ${supplier.name} - ${supplier.contact}")
+            }
+        }
+    }
+
+
+    fun addSupplieruser(api: SupplierAPI) {
+        print("Supplier ID: ")
+        val id = readln().toInt()
+
+        print("Name: ")
+        val name = readln()
+
+        print("Contact: ")
+        val contact = readln()
+
+    }
+
+    fun deleteSupplier(id: Int): Boolean {
+        getAllSuppliers()
+        for (supplier in suppliers) {
+            if (supplier.supplierId == id) {
+                suppliers.remove(supplier)
+                return true
+            }
+        }
+        return false
+    }
+
+
 }
 
-fun addSupplier(api: SupplierAPI) {
-    print("Supplier ID: ")
-    val id = readln().toInt()
 
-    print("Name: ")
-    val name = readln()
 
-    print("Contact: ")
-    val contact = readln()
-
-}
