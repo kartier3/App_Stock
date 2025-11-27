5911832cd4c2d45eeae8796d3a1f64900051181b
kartier3 <136193694+kartier3@users.noreply.github.com>
2025-11-27 15:43:16 +0000

    Added a menu

--- src/main/kotlin/Controllers/ProductAPI.kt
+++ src/main/kotlin/Controllers/ProductAPI.kt
@@ -5,7 +5,21 @@ class ProductAPI(private val supplierAPI: SupplierAPI) {
 
     private val products = mutableListOf<Product>()
 
-    fun addProduct(product: Product): Boolean {return products.add(product) }
+    fun addProduct(product: Product): Boolean { return products.add(product)
+    }
 
     fun getAllProducts(): List<Product> {return products }
+}
+fun addProduct(api: ProductAPI) {
+    print("Product ID: ")
+    val id = readln().toInt()
+
+    print("Name: ")
+    val name = readln()
+
+    print("Cost: ")
+    val cost = readln().toDouble()
+
+    print("Supplier ID: ")
+    val supplierId = readln().toInt()
 }
