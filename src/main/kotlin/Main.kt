4d9d61ee88a217721047fe3e1b85131c0e17837c
kartier3 <136193694+kartier3@users.noreply.github.com>
2026-02-03 21:09:46 +0000

    Minor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -26,6 +26,7 @@ fun main() {
 
     do {
         println()
+        println()
         println("┌──────────────────────────┐")
         println("│        MAIN MENU         │")
         println("├──────────────────────────┤")
